18afb512bbc8e132564b0bf29a896582f12466cf
Bhavin Patel <bhavin.2304patel@gmail.com>
2025-08-08 11:16:22 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [steps, setSteps] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const handleRestart = () => {
+    setCount(0);
+    setSteps(1);
+  };
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="7"
+          value={steps}
+          onChange={(e) => setSteps(Number(e.target.value))}
+        />
+        <span>Steps: {steps}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - steps)}>-</button>
+        <span>
+          <input
+            type="number"
+            value={Math.abs(count)}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
+        </span>
+        <button onClick={() => setCount((c) => c + steps)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+      {count !== 0 || steps !== 1 ? (
+        <div>
+          <button onClick={handleRestart}>Restart</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
